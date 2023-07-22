403e6afdb87b678b3ec8f9843c0d6f470ed90584
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-22 21:04:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,6 +18,9 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
+import pytz
+from datetime import datetime
+
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
